7520db1a742cc69796e4797b30f82359527c1925
ZAKARYAELOUKILI <zakaryae.loukili@hotmail.com>
2025-02-18 23:22:25 +0100

    ajouter login et projet 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
       <nav className="navbar">
         <h1> codv Blog</h1>
-        <div className="links" >
-            <Link to="/"> <button style={{padding :"10px"}}>Home</button> </Link>
-            <Link to="/create"> <button  style={{padding :"10px"}}>New Post</button></Link>
+        <div className="links">
+          <Link to="/"> <button style={{ padding: "10px" }}>Home</button> </Link>
+
+          <Link to="/create">  <button style={{ padding: "10px" }}>New Post</button></Link>
+
+          <Link to="/zzz"> <button style={{ padding: "20px" ,marginLeft:"100px" }}>Projet 2 avec useReducer </button></Link>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
